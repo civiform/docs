1d71d94a50b4442494c0ab25416c8963d44fa921
rasmi <rasmi@google.com>
2022-05-15 23:14:27 -0400

    Use prettier to format HTML.

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -1,5 +1,7 @@
+const path = require("path");
 const esbuild = require('esbuild')
 const { sassPlugin } = require("esbuild-sass-plugin");
+const prettier = require("prettier");
 
 
 module.exports = function(eleventyConfig) {
@@ -20,6 +22,16 @@ module.exports = function(eleventyConfig) {
   });
   eleventyConfig.addWatchTarget("./src/js/");
   eleventyConfig.addWatchTarget("./src/css/");
+
+  eleventyConfig.addTransform("prettier", function (content, outputPath) {
+    const extname = path.extname(outputPath);
+    switch (extname) {
+      case ".html":
+        return prettier.format(content, { parser: 'html' });
+      default:
+        return content;
+    }
+  });
 
   eleventyConfig.addPassthroughCopy({ "src/rootcopy": "/" });
   eleventyConfig.addPassthroughCopy({ "src/assets/img": "img" });
